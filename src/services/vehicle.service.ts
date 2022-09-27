7fc6f83dd8f48cf873e03c5820ffb21e684c3f7c
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-09-27 16:41:06 +0700

    fix: [EP-212] incorrect email validation

--- src/services/vehicle.service.ts
+++ src/services/vehicle.service.ts
@@ -1,7 +1,8 @@
+import { VehicleTypeModel } from "@/modules/vehicle-management/models";
 import {
   Vehicle,
   VehicleSelection,
-  VehicleType,
+  // VehicleType,
   VehicleDetail
 } from "@/modules/vehicle-management/models/vehicle.model";
 import { transformRequest } from "./base.service";
@@ -12,36 +13,36 @@ const data: VehicleDetail[] = [
     type: "2T Truck",
     name: "2T Car 1",
     numberPlate: "Kagoshima 100Ah 11-11",
-    maxWeight:4,
-    code:'XD1234',
-    industrial:true
+    maxWeight: 4,
+    code: "XD1234",
+    industrial: true
   },
   {
     key: "2",
     type: "3T Truck",
     name: "2T Car 1",
     numberPlate: "Kagoshima 100Ah 11-11",
-    maxWeight:4,
-    code:'XD1235',
-    industrial:true
+    maxWeight: 4,
+    code: "XD1235",
+    industrial: true
   },
   {
     key: "3",
     type: "4T Truck",
     name: "2T Car 1",
     numberPlate: "Kagoshima 100Ah 11-11",
-    maxWeight:4,
-    code:'XD1236',
-    industrial:true
+    maxWeight: 4,
+    code: "XD1236",
+    industrial: true
   },
   {
     key: "4",
     type: "5T Truck",
     name: "2T Car 1",
     numberPlate: "Kagoshima 100Ah 11-11",
-    maxWeight:4,
-    code:'XD1237',
-    industrial:true
+    maxWeight: 4,
+    code: "XD1237",
+    industrial: true
   }
 ];
 
@@ -64,21 +65,21 @@ export function getMockPartner(): VehicleSelection[] {
   return res;
 }
 
-export function getVehicleTypes(): VehicleType[] {
+export function getVehicleTypes(): VehicleTypeModel[] {
   // const [error, res] = await transformRequest({
   //   url: "",
   //   method: "get",
   //   params: { pageSize, pageIndex }
   // });
   //if (error) return undefined;
-  const res: VehicleType[] = [];
-  for (let i = 0; i < 20; i++) {
-    res.push({
-      id: `v${i}`,
-      key: i,
-      name: `Edward King ${i}`
-    });
-  }
+  const res: VehicleTypeModel[] = [];
+  // for (let i = 0; i < 20; i++) {
+  //   res.push({
+  //     id: `v${i}`,
+  //     key: i,
+  //     name: `Edward King ${i}`
+  //   });
+  // }
   return res;
 }
 
@@ -114,22 +115,20 @@ export async function createVehicle(
 export async function updateVehicle(
   vehicleInfo: Vehicle
 ): Promise<Vehicle | unknown> {
-  const { vehicleType, vehicleName, vehiclePlate, maxWeight, code,id } =
+  const { vehicleType, vehicleName, vehiclePlate, maxWeight, code, id } =
     vehicleInfo;
   const [error, res] = await transformRequest({
     url: `update-vehicle/${id}`,
     method: "patch",
-    params:{
-
-    },
+    params: {},
     data: { vehicleType, vehicleName, vehiclePlate, maxWeight, code }
   });
   if (error) return undefined;
   return res;
 }
 
-export function getVehicleById(id:string): VehicleDetail | undefined {
-  const vehicle = data.find((item) => item.key === id) || undefined
+export function getVehicleById(id: string): VehicleDetail | undefined {
+  const vehicle = data.find((item) => item.key === id) || undefined;
 
-  return vehicle
+  return vehicle;
 }
