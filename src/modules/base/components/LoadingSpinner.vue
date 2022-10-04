1f0ab86a79af3f88dd1e196bb65606158ecdbc44
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-10-04 11:03:06 +0700

    fix: recreate MR for EP-27

---
+++ src/modules/base/components/LoadingSpinner.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="loading-spinner">
+    <div
+      v-for="number in 8"
+      :key="number"
+      :style="`background-color: ${backgroundColor};`"
+    ></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  backgroundColor: {
+    type: String,
+    default: "#07A0B8"
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.loading-spinner {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 60px;
+
+  div {
+    transform-origin: 40px 30px;
+    animation: spinner 0.8s linear infinite;
+
+    &::after {
+      content: " ";
+      display: block;
+      position: absolute;
+      top: 3px;
+      left: 37px;
+      width: 3px;
+      height: 14px;
+      border-radius: 8px;
+      background-color: inherit;
+    }
+
+    &:nth-child(1) {
+      transform: rotate(0deg);
+      animation-delay: -0.7s;
+    }
+    &:nth-child(2) {
+      transform: rotate(45deg);
+      animation-delay: -0.6s;
+    }
+    &:nth-child(3) {
+      transform: rotate(90deg);
+      animation-delay: -0.5s;
+    }
+    &:nth-child(4) {
+      transform: rotate(135deg);
+      animation-delay: -0.4s;
+    }
+    &:nth-child(5) {
+      transform: rotate(180deg);
+      animation-delay: -0.3s;
+    }
+    &:nth-child(6) {
+      transform: rotate(225deg);
+      animation-delay: -0.2s;
+    }
+    &:nth-child(7) {
+      transform: rotate(270deg);
+      animation-delay: -0.1s;
+    }
+    &:nth-child(8) {
+      transform: rotate(315deg);
+      animation-delay: 0s;
+    }
+  }
+}
+
+@keyframes spinner {
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+}
+</style>
---
+++ src/modules/base/models/search-header.model.ts
@@ -0,0 +1,3 @@
+export default interface SearchHeaderRef {
+  clearInput: () => void;
+}
