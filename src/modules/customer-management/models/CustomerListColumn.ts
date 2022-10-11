fd9053462763ced74bb98ed0317493e94f12b1d1
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-10-11 17:12:20 +0700

    feat: EP-71-View Customer list

---
+++ src/modules/customer-management/models/CustomerListColumn.ts
@@ -0,0 +1,33 @@
+import { i18n } from "@/i18n";
+
+export const columns = [
+  {
+    title: i18n.global.t("name"),
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: i18n.global.t("postal_code"),
+    dataIndex: "postalCode",
+    key: "postalCode"
+  },
+  {
+    title: i18n.global.t("address"),
+    dataIndex: "address",
+    key: "address"
+  },
+  {
+    title: i18n.global.t("phone_number"),
+    dataIndex: "phoneNumber",
+    key: "phoneNumber"
+  },
+  {
+    title: i18n.global.t("email"),
+    dataIndex: "email",
+    key: "email"
+  },
+  {
+    key: "action",
+    dataIndex: "action"
+  }
+];
