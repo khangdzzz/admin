dcc5cfce8303c8183e78a8f2f0a6c6de35ca2ee3
thiago_bui <thiago.bui@techvify.com.vn>
2022-10-14 09:25:49 +0700

    feat: integrate api view list container and delete contaienr

--- src/modules/container/models/container.model.ts
+++ src/modules/container/models/container.model.ts
@@ -17,3 +17,29 @@ export interface ContainerType {
   capaity?: string;
   type?: string;
 }
+
+export interface ContainerDetail {
+  weight: number
+  key?: number;
+  name: string;
+  container_type___name: string;
+  capacity: number
+}
+
+export interface ResContainer {
+  capacity: number;
+  container_type___name: string;
+  container_type_id: number
+  created_at: Date;
+  created_by: number;
+  deleted_at: Date | null
+  deleted_by: number | null
+  extension_code: string;
+  id: number;
+  name: string
+  tag_id: string | null;
+  tenant_id: number;
+  updated_at: Date | null;
+  updated_by: number | null;
+  weight: number
+}
