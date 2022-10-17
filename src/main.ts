f67ee671f156bf78dce6ddd453b5f15d6e606ac1
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-10-17 09:57:29 +0700

    fix: [EP-376] common spinner for whole application

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import { createApp } from "vue";
+import { createApp, h } from "vue";
 import App from "@/modules/base/App.vue";
 import { i18n } from "./i18n";
-import Antd from "ant-design-vue";
+import Antd, { Spin } from "ant-design-vue";
 import "ant-design-vue/dist/antd.css";
 import "ant-design-vue/dist/antd.less";
 import { router } from "./routes";
@@ -13,6 +13,7 @@ import mitt from "mitt";
 import MessengerParamModel from "@/modules/base/models/messenger-param.model";
 import OpenLayersMap from "vue3-openlayers";
 import "vue3-openlayers/dist/vue3-openlayers.css";
+import LoadingSpinnerVue from "./modules/base/components/LoadingSpinner.vue";
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -20,6 +21,16 @@ const emitter = mitt();
 const messenger = (param: MessengerParamModel): void => {
   emitter.emit("ShowModal", param);
 };
+
+//antdesign global config
+Spin.setDefaultIndicator({
+  indicator: h(LoadingSpinnerVue, {
+    style: {
+      fontSize: "60px"
+    },
+    spin: true
+  })
+});
 
 app.component(ScreenLayout.AUTH_LAYOUT, AuthLayout);
 app.component(ScreenLayout.DEFAULT_LAYOUT, DefaultLayout);
