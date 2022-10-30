a94227b5721ee4f7e8d9331a23e94747845da5f2
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-10-30 13:58:57 +0700

    fix: blank ui when token was expired

--- src/services/local-storage-keys.ts
+++ src/services/local-storage-keys.ts
@@ -2,5 +2,6 @@ export const localStorageKeys = {
   accessToken: "_ACCESS_TOKEN_KEY_",
   currentLanguage: "CURRENT_LANGUAGE",
   refreshToken: "_REFRESH_TOKEN_",
-  userType: "USER_TYPE_KEY"
+  userType: "USER_TYPE_KEY",
+  userName: "USER_NAME_KEY"
 };
