fcb94f3ba92473d287ac85252ac11fb57cc4a481
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-11-01 18:24:33 +0700

    fix: [EP-676] incorrect deleted fail collection point

--- src/modules/collection-point-management/pages/CollectionPointDetailPage.vue
+++ src/modules/collection-point-management/pages/CollectionPointDetailPage.vue
@@ -282,7 +282,7 @@ const onDeleteCollectionPoint = async (deleteId: number): Promise<void> => {
   if (!isSuccess) {
     messenger({
       title: "popup_delete_fail_lbl_title",
-      message: "popup_delete_fail_lbl_message",
+      message: "",
       type: MessengerType.Error
     });
     return;
