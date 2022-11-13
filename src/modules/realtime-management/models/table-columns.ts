b8f0a0f457f8ad6f4573f43f6926878ec1d70b16
Nghia Ha Minh <nghiahm@techvify.com.vn>
2022-11-13 14:49:32 +0700

    fix: [EP-1317] realtime screen some text were not translated after change language

--- src/modules/realtime-management/models/table-columns.ts
+++ src/modules/realtime-management/models/table-columns.ts
@@ -1,14 +1,12 @@
-import { i18n } from "@/i18n";
-
 export const listOfUserColumns = [
   {
-    title: i18n.global.t("username"),
+    title: "username",
     dataIndex: "userName",
     key: "userName",
     width: "180px"
   },
   {
-    title: i18n.global.t("last_active_time"),
+    title: "last_active_time",
     dataIndex: "lastUpdateTime",
     key: "lastUpdateTime",
     width: "190px"
@@ -22,7 +20,7 @@ export const listOfUserColumns = [
 
 export const listOfCollectionPointColumns = [
   {
-    title: i18n.global.t("name"),
+    title: "name",
     dataIndex: "name",
     key: "name"
   }
